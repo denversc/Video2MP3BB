f166f0b0c8cb3943454e1546ab2d579cdd07fbef
Denver Coneybeare <dconeybeare@rim.com>
2010-11-13 14:53:11 -0500

    Created skeleton

---
+++ src/main/java/video2mp3bb/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Main.java
+ *
+ * © <your company here>, <year>
+ * Confidential and proprietary.
+ */
+
+package src.main.java.video2mp3bb;
+
+
+
+
+/**
+ * 
+ */
+class Main {
+    Main() {    }
+} 
